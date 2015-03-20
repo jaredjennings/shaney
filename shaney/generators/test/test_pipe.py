8714d992eafd03b007b66460b2ef390f366347e1
Jared Jennings <jjennings@fastmail.fm>
2015-03-19 22:23:48 -0500

    apply GPLv3

--- shaney/generators/test/test_pipe.py
+++ shaney/generators/test/test_pipe.py
@@ -1,15 +1,19 @@
-# --- BEGIN DISCLAIMER ---
-# Those who use this do so at their own risk;
-# AFSEO does not provide maintenance nor support.
-# --- END DISCLAIMER ---
-# --- BEGIN AFSEO_DATA_RIGHTS ---
-# This is a work of the U.S. Government and is placed
-# into the public domain in accordance with 17 USC Sec.
-# 105. Those who redistribute or derive from this work
-# are requested to include a reference to the original,
-# at <https://github.com/afseo/cmits>, for example by
-# including this notice in its entirety in derived works.
-# --- END AFSEO_DATA_RIGHTS ---
+# shaney - prepare Puppet code with LaTeX comments for multiple audiences.
+# Based on <https://github.com/afseo/cmits>.
+# Copyright (C) 2015 Jared Jennings, jjennings@fastmail.fm.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import unittest
 from shaney.generators.test import Sink
 from shaney.generators import prime, pipe
